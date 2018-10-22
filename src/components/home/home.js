753bd64b454aaa32f79d0f0f712cb7bb0fd5b168
Tom <tnorth93@gmail.com>
2018-10-22 13:51:49 -0700

    added home

---
+++ src/components/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <section>
+        <p> Home </p>
+      </section>
+    );
+  }
+}
+
+export default Home;
